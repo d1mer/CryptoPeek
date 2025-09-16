9ea6b108a5228023d12ce4a4e3f25c63fd2beb8e
d1mer <d1mer@outlook.com>
2025-09-16 20:59:06 +0300

    Initial commit

---
+++ CryptoPeek/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CryptoPeek
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
