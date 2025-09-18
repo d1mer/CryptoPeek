8be0f97cb87f56f25735d7101705b3a7a5e8dc07
d1mer <d1mer@outlook.com>
2025-09-18 19:51:59 +0300

    prism package installed

--- CryptoPeek/App.xaml.cs
+++ CryptoPeek/App.xaml.cs
@@ -1,14 +1,22 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace CryptoPeek
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            var window = Container.Resolve<MainWindow>();
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 
 }
