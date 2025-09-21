61a23d805366bbacbe503f7d56b15b170c54518e
d1mer <d1mer@outlook.com>
2025-09-21 18:21:28 +0300

    add coinshortviewmodel and it's extension

---
+++ CryptoPeek/Extensions/CoinModelExtension.cs
@@ -0,0 +1,21 @@
+﻿using CryptoPeek.Models.Coin;
+
+namespace CryptoPeek.Extensions
+{
+    public static class CoinModelExtension
+    {
+        public static CoinShortViewModel ToCoinShortViewModel(this CoinModel coinModel)
+        {
+            return new CoinShortViewModel
+            {
+                Image = coinModel.Image,
+                Symbol = coinModel.Symbol,
+                Name = coinModel.Name,
+                CurrentPrice = coinModel.CurrentPrice,
+                PriceChangePercentage24h = coinModel.PriceChangePercentage24h,
+                MarketCap = coinModel.MarketCap,
+                TotalVolume = coinModel.TotalVolume,
+            };
+        }
+    }
+}
---
+++ CryptoPeek/Models/Coin/CoinViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CryptoPeek.Models.Coin
+{
+    public class CoinShortViewModel
+    {
+        public string Image { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+
+        public decimal? PriceChangePercentage24h { get; set; }
+
+        public decimal MarketCap { get; set; }
+
+        public decimal TotalVolume { get; set; }
+    }
+}
