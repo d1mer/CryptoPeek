ac395ac665766257e04286372ff660dd7c7a4ba8
d1mer <d1mer@outlook.com>
2025-09-25 21:30:32 +0300

    add expandable textblock

---
+++ CryptoPeek/Controls/ExpandableTextBlock.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoPeek.Controls
+{
+    public class ExpandableTextBlock : Control
+    {
+        static ExpandableTextBlock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ExpandableTextBlock), new FrameworkPropertyMetadata(typeof(ExpandableTextBlock)));
+        }
+
+        #region -- Public properties --
+
+        public static readonly DependencyProperty TextProperty = 
+            DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(ExpandableTextBlock),
+                new PropertyMetadata(string.Empty)
+                );
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty); 
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly DependencyProperty CollapsedLinesProperty =
+            DependencyProperty.Register(
+                nameof(CollapsedLines), 
+                typeof(int), 
+                typeof(ExpandableTextBlock), 
+                new PropertyMetadata(3));
+
+        public int CollapsedLines
+        {
+            get => (int)GetValue(CollapsedLinesProperty);
+            set => SetValue(CollapsedLinesProperty, value);
+        }
+
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register(
+                nameof(IsExpanded), 
+                typeof(bool), 
+                typeof(ExpandableTextBlock),
+                new PropertyMetadata(false));
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
+        }
+
+        #endregion
+    }
+}
---
+++ CryptoPeek/Converters/CollapsedHeightConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CryptoPeek.Converters
+{
+    public class CollapsedHeightConverter : IMultiValueConverter
+    {
+        // values [0] - IsExpanded (bool)
+        //        [1] - CollapsedLines (int)
+        //        [2] - FontSize (double)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isExpanded = default;
+            int lines = default;
+            double fontSize = default;
+
+            if (values.Length > 0 && values[0] is bool b) 
+            {
+                isExpanded = b;
+            }
+
+            if (values.Length > 1 && values[1] is int i)
+            {
+                lines = i > 0 ? i : 3;
+            }
+            else
+            {
+                lines = 3;
+            }
+
+            if (values.Length > 2 && values[2] is double d)
+            {
+                fontSize = d;
+            }
+            else
+            {
+                fontSize = 14.0;
+            }
+
+            return isExpanded
+                   ? double.PositiveInfinity
+                   : lines * fontSize * 1.35;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
